491da02e760955e73e9e67d88ef3a86644847e45
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2024-03-15 20:49:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from './components/pages/Home'
+import Products from './components/pages/Products';
+import Services from './components/pages/Services';
+import SignUp from './components/pages/SignUp';
+
 
 function App() {
   return (
@@ -9,7 +14,10 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' exact />
+          <Route path='/' exact Component={Home} />
+          <Route path='/services' exact Component={Services} />
+          <Route path='/products' exact Component={Products} />
+          <Route path='/sign-up' exact Component={SignUp} />
         </Routes>
       </Router>
     </>
