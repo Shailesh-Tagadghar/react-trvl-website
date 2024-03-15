9978e393c01bcc4ef39eae6213d39a157a2d0443
Shailesh Tagadghar <128667791+Shailesh-Tagadghar@users.noreply.github.com>
2024-03-15 20:51:28 +0530

    Pages in Components

---
+++ src/components/pages/Products.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../../App.css'
+import Footer from '../Footer';
+
+export default function Products() {
+  // return <h1 className='products'>PRODUCTS</h1>;
+  return (
+    <>
+    {/* <HeroSection /> */}
+    {/* <Cards /> */}
+    <h1 className='products'>PRODUCTS</h1>
+    <Footer />
+    </>
+)
+}
+
+
---
+++ src/components/pages/Services.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../App.css';
+import Footer from '../Footer';
+
+export default function Services() {
+  // return <h1 className='services'>SERVICES</h1>;
+  return (
+    <>
+    {/* <HeroSection /> */}
+    {/* <Cards /> */}
+    <h1 className='services'>SERVICES</h1>
+    <Footer />
+    </>
+  )
+}
